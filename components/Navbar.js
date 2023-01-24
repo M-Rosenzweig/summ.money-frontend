6fb81d0deb665d9a8ee0cbcb449daa13a2bead59
Moshe <moisherosenzweig@gmail.com>
2023-01-24 15:12:27 -0500

    Navbar 1.0

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav id="globalVibes" className="p-5 border-b-2 flex flex-row justify-between items-center">
-      <h1 className="py-4 px-4 font-bold text-3xl">Summ.money</h1>
+      <h1 className="py-4 px-4 font-bold text-3xl text-blue-light">Summ</h1>
       <div className="flex flex-row items-center">
         <Link href="/">
           <a className="mr-4 p-6">My Summs</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      blue: {
+        light: '#85d7ff'
+      }
+    }
   },
   plugins: [],
 };

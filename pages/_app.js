44f70793cf0dee23c4af34bf158883113ff0492a
Moshe <moisherosenzweig@gmail.com>
2023-01-24 11:12:35 -0500

    inital commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import Head from "next/head";
+// import styles from "@/styles/Home.module.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Summ.money</title>
+        <meta name="description" content="Summ.money" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default App;

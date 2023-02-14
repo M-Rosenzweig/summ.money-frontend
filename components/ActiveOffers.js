476a2517b562136d3d60d8a6e64b336cf815a971
Moshe <moisherosenzweig@gmail.com>
2023-02-14 15:24:58 -0500

    boxes for data on top

--- components/ActiveOffers.js
+++ components/ActiveOffers.js
@@ -2,22 +2,20 @@ import React from "react";
 import TopInfoBoxes from "@/components/TopInfoBoxes";
 
 function ActiveOffers({ summary, account }) {
-  console.log(summary);
-  console.log(account);
+
 
   return (
     <>
       <div className="h-screen flex flex-col">
-        <div className="h-1/5 w-screen flexChild bg-red-500">
+        <div className="h-1/5 w-screen flexChild overflow-x-auto">
           {Object.entries(summary).map(([key, value]) => {
             if (value !== false && key !== "termsStatus") {
-              // return <p>{`${key}:${value}`}</p>;
               return <TopInfoBoxes key={key} termKey={key} value={value.toString()} />;
             }
             return null;
           })}
         </div>
-        <div className="h-4/5 w-screen bg-blue-500">hello bottom</div>
+        <div className="h-4/5 w-screen bg-blue-50">hello bottom</div>
       </div>
     </>
   );
--- components/TopInfoBoxes.js
+++ components/TopInfoBoxes.js
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function TopInfoBoxes({ termKey, value }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (termKey == "opponent") {
+      setDescription("The address of the opponent in this Summ.");
+
+      setTitle("Opponent");
+    } else if (termKey == "totalSoftOfferCap") {
+      setDescription("The maximum amount of soft offer rounds.");
+
+      setTitle("Soft Offer Cap");
+    } else if (termKey == "totalFirmOfferCap") {
+      setDescription("The maximum amount of firm offers rounds.");
+
+      setTitle("Firm Offer Cap");
+    } else if (termKey == "softRange") {
+      setDescription("The percentage range difference you can compromise on in soft rounds.");
+
+      setTitle("Soft Range");
+    } else if (termKey == "firmRange") {
+      setDescription("The percentage range difference you can compromise on in firm rounds.");
+      setTitle("Firm Range");
+    } else if (termKey == "penaltyPercent") {
+      setDescription("The percentage used for penalties in firm round of average number.");
+
+      setTitle("Penalty Percent");
+    } else if (termKey == "creator") {
+      setDescription("The address of the creator of the negotiation.");
+      setTitle("Creator");
+    }
+  }, [termKey]);
+
   let ternaryValue =
     termKey == "opponent" || termKey == "creator"
       ? value.slice(0, 5) + "..." + value.slice(-5)
@@ -11,9 +44,9 @@ function TopInfoBoxes({ termKey, value }) {
       : ternaryValue;
 
   return (
-    <div className="m-4">
-      <p>{termKey}</p>
-      <p>{ternaryValue}</p>
+    <div className="m-8 bg-blue-100 p-4 rounded-lg">
+      <p>{title}</p>
+      <p className="text-center">{ternaryValuePercent}</p>
     </div>
   );
 }
